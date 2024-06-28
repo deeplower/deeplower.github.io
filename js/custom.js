03954b601b1e6302c806b9bf24cd837e130f24e1
deeplower <deeplower@163.com>
2024-06-28 15:43:07 +0800

    Site updated: 2024-06-28 15:43:05

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
-// document.addEventListener('DOMContentLoaded', function() {
+// document.addEventListener('DOMContentLoaded', function() 
+// {
 //     console.log('Custom.js file loaded');
 //     console.log('DOM fully loaded and parsed');
 
@@ -11,21 +12,12 @@
 //         console.error('Toggle button with id "toggle-background" not found.');
 //         return;
 //     }
-
-//     toggleButton.addEventListener('click', function() {
-//         console.log('Button clicked!'); // 点击按钮时输出信息
-
 //         var body = document.body;
 //         if (isImageVisible) {
-//             console.log('Switching to background color');
-//             // 切换为背景颜色
 //             body.style.background = '#696969';
 //         } else {
-//             console.log('Switching to background image');
-//             // 切换为背景图片
-//             // body.style.background = 'url(./img/Ganyu.jpg) no-repeat 0% 0%/ cover';
-//             body.style.background = 'url(https://img-blog.csdnimg.cn/direct/c00a24fe9905401cbe3229d7464ff72b.jpeg) no-repeat 0% 0%/ cover';
+//             body.style.background = 'url(./img/Ganyu.jpg) no-repeat 0% 0%/ cover';
 //         }
 //         isImageVisible = !isImageVisible; // 切换状态
-//     });
-// });
+//     }
+// );
