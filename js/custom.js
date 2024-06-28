4d1abdbc190fb95b9a9e6295d8f2685d640a7c78
deeplower <deeplower@163.com>
2024-06-28 18:35:50 +0800

    Site updated: 2024-06-28 18:35:48

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,38 @@
-// document.addEventListener('DOMContentLoaded', function() 
-// {
-//     console.log('Custom.js file loaded');
-//     console.log('DOM fully loaded and parsed');
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Custom.js 文件已加载');
+    console.log('DOM 已完全加载和解析');
 
-//     var isImageVisible = true; // 初始背景是否为图片，根据需求设置
+    var isImageVisible = true; // 初始背景是否为图片
 
-//     var toggleButton = document.getElementById('toggle-background');
-//     console.log('Toggle button element:', toggleButton); // 输出按钮元素，检查是否为 null
+    var toggleButton = document.getElementById('toggle-background');
+    console.log('切换按钮元素：', toggleButton);
 
-//     if (!toggleButton) {
-//         console.error('Toggle button with id "toggle-background" not found.');
-//         return;
-//     }
-//         var body = document.body;
-//         if (isImageVisible) {
-//             body.style.background = '#696969';
-//         } else {
-//             body.style.background = 'url(./img/Ganyu.jpg) no-repeat 0% 0%/ cover';
-//         }
-//         isImageVisible = !isImageVisible; // 切换状态
-//     }
-// );
+    if (!toggleButton) {
+        console.error('未找到 id 为 "toggle-background" 的切换按钮。');
+        return;
+    }
+
+    var opacitySlider = document.querySelector('.opacity-slider');
+    if (!opacitySlider) {
+        console.error('未找到类名为 "opacity-slider" 的滑块元素。');
+        return;
+    }
+
+    toggleButton.addEventListener('click', function() {
+        var body = document.body;
+
+        // 切换背景
+        if (isImageVisible) {
+            body.style.background = '#696969';
+            opacitySlider.style.display = 'none'; // 隐藏滑块
+        } else {
+            // body.style.background = 'url(htps://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
+            // body.style.background = 'url(' + baseUrl + '/img/Ganyu.jpg) no-repeat 0% 0%/ cover';
+            body.style.background = 'url(https://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
+            opacitySlider.style.display = 'block'; // 显示滑块
+        }
+
+        // 切换状态
+        isImageVisible = !isImageVisible;
+    });
+});
