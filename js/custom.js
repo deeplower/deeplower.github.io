d00752474410e815735229b2620ab14868a5e111
deeplower <deeplower@163.com>
2024-06-28 14:21:07 +0800

    Site updated: 2024-06-28 14:21:04

---
+++ js/custom.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Custom.js file loaded');
+    console.log('DOM fully loaded and parsed');
+
+    var isImageVisible = true; // 初始背景是否为图片，根据需求设置
+
+    var toggleButton = document.getElementById('toggle-background');
+    console.log('Toggle button element:', toggleButton); // 输出按钮元素，检查是否为 null
+
+    if (!toggleButton) {
+        console.error('Toggle button with id "toggle-background" not found.');
+        return;
+    }
+
+    toggleButton.addEventListener('click', function() {
+        console.log('Button clicked!'); // 点击按钮时输出信息
+
+        var body = document.body;
+        if (isImageVisible) {
+            console.log('Switching to background color');
+            // 切换为背景颜色
+            body.style.background = '#696969';
+        } else {
+            console.log('Switching to background image');
+            // 切换为背景图片
+            body.style.background = 'url(./img/Ganyu.jpg) no-repeat 0% 0%/ cover';
+        }
+        isImageVisible = !isImageVisible; // 切换状态
+    });
+});
