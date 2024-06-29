7b4a843e128d21d9d4266b9526e5b222b11c0906
deeplower <deeplower@163.com>
2024-06-30 03:48:03 +0800

    Site updated: 2024-06-30 03:48:01

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     console.log('Custom.js 文件已加载');
     console.log('DOM 已完全加载和解析');
 
     var isImageVisible = true; // 初始背景是否为图片
+    var flag = 1;
 
     var toggleButton = document.getElementById('toggle-background');
     console.log('切换按钮元素：', toggleButton);
@@ -18,7 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         return;
     }
 
-    toggleButton.addEventListener('click', function() {
+    toggleButton.addEventListener('click', function () {
         var body = document.body;
 
         // 切换背景
@@ -26,12 +27,32 @@ document.addEventListener('DOMContentLoaded', function() {
             body.style.background = '#696969';
             opacitySlider.style.display = 'none'; // 隐藏滑块
         } else {
-            // body.style.background = 'url(htps://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
-            // body.style.background = 'url(' + baseUrl + '/img/Ganyu.jpg) no-repeat 0% 0%/ cover';
-            body.style.background = 'url(https://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
-            opacitySlider.style.display = 'block'; // 显示滑块
+            if (flag == 1) {
+                body.style.background = 'url(https://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
+                opacitySlider.style.display = 'block'; // 显示滑块
+                flag = flag + 1;
+            }
+            else if (flag == 2) {
+                body.style.background = 'url(https://deeplower.github.io/about/index/Bronya.jpg) no-repeat 0% 0%/ cover';
+                opacitySlider.style.display = 'block'; // 显示滑块
+                flag = flag + 1;
+            }
+            else if (flag == 3) {
+                body.style.background = 'url(https://deeplower.github.io/about/index/Shenzi.jpg) no-repeat 0% 0%/ cover';
+                opacitySlider.style.display = 'block'; // 显示滑块
+                flag = flag + 1;
+            }
+            else if (flag == 4) {
+                body.style.background = 'url(https://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
+                opacitySlider.style.display = 'block'; // 显示滑块
+                flag = flag + 1;
+            }
+            else {
+                body.style.background = 'url(https://deeplower.github.io/about/index/Ganyu.jpg) no-repeat 0% 0%/ cover';
+                opacitySlider.style.display = 'block'; // 显示滑块
+                flag = 1;
+            }
         }
-
         // 切换状态
         isImageVisible = !isImageVisible;
     });
